0b01b73ffd4f0e9980f09bb858742d616cc9a3d1
Olivier Swaak <o.swaak@hotmail.com>
2026-04-23 14:57:47 +0200

    offline: fix: use relative path for lnk target
    Linking to absolute path assumes you're still on the same system as the .lnk was created on, so this hopefully links to the correct location on the fly.

--- build-tools/hedy-offline/create-shortcut-windows.ps1
+++ build-tools/hedy-offline/create-shortcut-windows.ps1
@@ -1,4 +1,4 @@
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut(".\dist\offlinehedy\Hedy.lnk")
-$shortcut.TargetPath = [System.IO.Path]::GetFullPath("dist\offlinehedy\run-hedy-server.exe")
+$shortcut.TargetPath = "%COMSPEC% /C start run-hedy-server.exe"
 $shortcut.Save()
