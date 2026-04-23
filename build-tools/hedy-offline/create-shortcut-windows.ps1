fb021228d182652222138852d84c14e178a3199e
Olivier Swaak <o.swaak@hotmail.com>
2026-04-23 14:30:02 +0200

    offline: fix: shortcut correct reference path

--- build-tools/hedy-offline/create-shortcut-windows.ps1
+++ build-tools/hedy-offline/create-shortcut-windows.ps1
@@ -1,4 +1,4 @@
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut(".\dist\offlinehedy\Hedy.lnk")
-$shortcut.TargetPath = ".\dist\offlinehedy\run-hedy-server.exe"
+$shortcut.TargetPath = [System.IO.Path]::GetFullPath("dist\offlinehedy\run-hedy-server.exe")
 $shortcut.Save()
